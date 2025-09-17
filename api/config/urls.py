bc4fe15118dabd8d7133cc6bf5be7d58f88e7dd9
jmgalindor9802 <jmgalindor9802@gmail.com>
2025-09-17 00:59:17 -0500

    feat(api): Configuracion de router for API viewsets

--- api/config/urls.py
+++ api/config/urls.py
@@ -16,8 +16,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from core.views import (
+    BrandViewSet,
+    CategoryViewSet,
+    CustomerViewSet,
+    OrderItemViewSet,
+    OrderViewSet,
+    PaymentViewSet,
+    ProductViewSet,
+    StockViewSet,
+    WarehouseViewSet,
+)
+
+router = DefaultRouter()
+router.register("brands", BrandViewSet)
+router.register("categories", CategoryViewSet)
+router.register("products", ProductViewSet)
+router.register("warehouses", WarehouseViewSet)
+router.register("stocks", StockViewSet)
+router.register("customers", CustomerViewSet)
+router.register("orders", OrderViewSet)
+router.register("order-items", OrderItemViewSet)
+router.register("payments", PaymentViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
